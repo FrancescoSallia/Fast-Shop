d4bdfc4d3df892f5f0f9a5755f6038553f6df2e4
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-01-08 13:42:15 +0100

    Test
    Signed-off-by: FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>

---
+++ Fast-Shop/Fast-Shop/Fast_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fast_ShopApp.swift
+//  Fast-Shop
+//
+//  Created by Francesco Sallia on 08.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Fast_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
