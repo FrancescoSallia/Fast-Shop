02a0b4da9937b634cc2e58085384a26c643f0efa
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-01-16 14:48:54 +0100

    WIP UI

--- Fast-Shop/Fast-Shop/Views/HomeView.swift
+++ Fast-Shop/Fast-Shop/Views/HomeView.swift
@@ -13,76 +13,78 @@ struct HomeView: View {
     var randomList = ["text1", "text2", "text3", "text4", "text5"]
 
     var body: some View {
-        
-        
         NavigationStack {
-        VStack {
-//            Text("Fast Shop")
-//                .fontDesign(.monospaced)
-//                .padding()
-            
-            Text("Ihre Lieblingsmarken \n in einer App!")
-                .font(.title)
-                .fontDesign(.serif)
-                .frame(height: 100)
-        }
-     
-            
-            VStack {
-                Text("TOP BRANDS")
-                    .fontDesign(.default)
-                    .font(.title3)
-            }
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(randomList, id: \.self) { item in
-                        VStack {
-                            Image("tshirt")
-                                .resizable()
-                                .scaledToFit()
-                                .clipShape(.circle)
-                                .frame(width: 100, height: 100)
-                                .shadow(radius: 2, x: 2, y: 2)
+            ScrollView {
+                VStack {
+                    //            Text("Fast Shop")
+                    //                .fontDesign(.monospaced)
+                    //                .padding()
+                
+                    Text("Ihre Lieblingsmarken \n in einer App!")
+                        .font(.title)
+                        .fontDesign(.serif)
+                        .frame(height: 100)
+                }
+        
+                
+                VStack {
+                    Text("TOP BRANDS")
+                        .fontDesign(.default)
+                        .font(.title3)
+                }
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(randomList, id: \.self) { item in
+                            VStack {
+                                Image("tshirt")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .clipShape(.circle)
+                                    .frame(width: 100, height: 100)
+                                    .shadow(radius: 2, x: 2, y: 2)
 
-                            Text("Title")
-                                .fontDesign(.serif)
-                                .font(.footnote)
+                                Text("Title")
+                                    .fontDesign(.serif)
+                                    .font(.footnote)
+                            }
                         }
                     }
+                    .padding(.horizontal)
                 }
-            }
-            TabView {
-                Image("tasche")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                Image("ring")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-                Image("tshirt")
-                    .resizable()
-                    .scaledToFit()
-                    .padding()
-            }
-            .tabViewStyle(.page)
-            .indexViewStyle(.page(backgroundDisplayMode: .always))
-            .frame(width: 400, height: 300)
-            .padding()
-
-            VStack {
-                List(randomList, id: \.self) { item in
+                TabView {
+                    Image("tasche")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
                     Image("ring")
                         .resizable()
                         .scaledToFit()
                         .padding()
+                    Image("tshirt")
+                        .resizable()
+                        .scaledToFit()
+                        .padding()
                 }
-                .listStyle(.inset)
-                
+                .tabViewStyle(.page)
+                .indexViewStyle(.page(backgroundDisplayMode: .always))
+                .frame(width: 400, height: 300)
+                .padding()
+
+                VStack {
+                    ForEach(randomList, id: \.self) { item in
+                        Image("ring")
+                            .resizable()
+                            .scaledToFit()
+                            .padding()
+                    }
+                    .listStyle(.inset)
+                    
+                }
+                .padding()
+                .navigationTitle("Fast Shop")
+                .navigationBarTitleDisplayMode(.inline)
+                .searchable(text: $search, prompt: "Suche...")
             }
-            .navigationTitle("Fast Shop")
-            .navigationBarTitleDisplayMode(.inline)
-            .searchable(text: $search, prompt: "Suche...")
         }
     }
 }
