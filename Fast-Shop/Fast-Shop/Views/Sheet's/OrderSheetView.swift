6e94c840749e912d2316aa0274283aa330d9def5
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-02-01 15:40:46 +0100

    OrderView erstellt WIP

---
+++ Fast-Shop/Fast-Shop/Views/Sheet's/OrderSheetView.swift
@@ -0,0 +1,105 @@
+//
+//  OrderSheetView.swiftColor("AccentColor")
+//  Fast-Shop
+//
+//  Created by Francesco Sallia on 01.02.25.
+//
+
+import SwiftUI
+
+struct OrderSheetView: View {
+    
+    var body: some View {
+        VStack {
+            HStack {
+                ForEach(0...1, id: \.self) { item in
+                    ZStack {
+                        Rectangle()
+                            .frame(width: 206, height: 100)
+                            .foregroundStyle(.white)
+                            .border(Color.black)
+                            .padding(-4)
+                        VStack {
+                            Image(systemName: "map.fill")
+                            Text("Lieferstelle")
+                        }
+                    }
+                }
+            }
+            ZStack {
+                Rectangle()
+                    .frame(width: .infinity, height: 60)
+                    .foregroundStyle(.white)
+                    .border(Color.primary)
+                    .padding(-6)
+                HStack {
+                    Text("Musterstraße 123") //Als NavigationLink
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            
+            HStack {
+                Text("ZUSTELLUNG")
+                    .font(.footnote)
+                    .padding()
+                Spacer()
+            }
+            HStack {
+                Image("ring")
+                    .resizable()
+                    .frame(width:110, height: 140)
+                Image("ring")
+                    .resizable()
+                    .frame(width:110, height: 140)
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            VStack {
+            HStack {
+                    Image(systemName: "button.programmable")
+                    Text("DIENSTAG 04, FEBRUAR - MITTWOCH 05, FEBRUAR")
+                        .font(.footnote)
+                Spacer()
+                    Text("KOSTENLOS")
+                    .font(.footnote)
+
+                Spacer()
+                }
+            .padding(.top)
+            .padding(.horizontal)
+            HStack {
+                Image(systemName: "button.programmable")
+                Text("MONTAG 03, FEBRUAR - DIENSTAG 04, FEBRUAR")
+                    .font(.footnote)
+                Spacer()
+                Text("8,95 EUR")
+                    .font(.footnote)
+                Spacer()
+
+                }
+            .padding(.horizontal)
+            }
+        }
+        ZStack {
+            Rectangle()
+                .frame(width: .infinity, height: 50)
+                .foregroundStyle(.white)
+                .border(Color.black)
+            HStack {
+                Text("VERSAND")
+                    .font(.footnote)
+                Spacer()
+                Text("KOSTENLOS")
+                    .font(.footnote)
+            }
+            .padding(.horizontal)
+            .bold()
+        }
+    }
+}
+
+#Preview {
+    OrderSheetView()
+}
