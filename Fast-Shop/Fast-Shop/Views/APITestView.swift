39a72b2cd84baf2fc3eb474b03446b56771b8e25
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-02-03 21:10:49 +0100

    WIP

--- Fast-Shop/Fast-Shop/Views/APITestView.swift
+++ Fast-Shop/Fast-Shop/Views/APITestView.swift
@@ -13,10 +13,18 @@ struct APITestView: View {
 
     var body: some View {
         ZStack {
-                    VideoBackgroundView()
-                        .ignoresSafeArea() // Deckt den ganzen Bildschirm ab
+            VideoBackgroundView()
+                .ignoresSafeArea() // Deckt den ganzen Bildschirm ab
             
             VStack {
+                
+                VStack (alignment: .leading){
+                    Text("FAST-SHOP")
+                        .fontDesign(.serif)
+                        .font(.largeTitle)
+                        .padding(.top, -200)
+                }
+                
           TextField("E-Mail", text: .constant(""))
                     .padding()
                     .background(Color(uiColor: UIColor.lightText))
@@ -38,31 +46,35 @@ struct APITestView: View {
               .font(.subheadline)
               .padding(.bottom, 20)
               .padding(.leading, 8)
+              .padding(.top, 4)
                   Spacer()
           }
                 
-                Button(action: {
-                  //placeholder
-                }) {
-                    Text("Anmelden")
-                        .textCase(.uppercase)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.black.opacity(0.8))
-                        .cornerRadius(3)
+                VStack {
+                    Button(action: {
+                      //placeholder
+                    }) {
+                        Text("Anmelden")
+                            .textCase(.uppercase)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.black.opacity(0.85))
+                            .cornerRadius(3)
+                    }
+                    Button(action: {
+                      //placeholder
+                    }) {
+                        Text("Registrieren")
+                            .textCase(.uppercase)
+                            .foregroundColor(.black)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.white.opacity(0.9))
+                            .cornerRadius(3)
+                    }
                 }
-                Button(action: {
-                  //placeholder
-                }) {
-                    Text("Registrieren")
-                        .textCase(.uppercase)
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.white.opacity(0.9))
-                        .cornerRadius(3)
-                }
+                .padding(.top, 28)
             }
             .padding()
             .padding(.top, 80)
