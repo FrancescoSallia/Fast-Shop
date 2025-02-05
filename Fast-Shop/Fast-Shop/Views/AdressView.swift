770a9f4ac026519cab688e138642a842f0d38bb3
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-02-05 16:06:21 +0100

    AdressView WIP

--- Fast-Shop/Fast-Shop/Views/AdressView.swift
+++ Fast-Shop/Fast-Shop/Views/AdressView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AdressView: View {
     
     @ObservedObject var viewModel: ProductViewModel
+    @State var showTextFields: Bool = false
     
     var body: some View {
         ZStack {
@@ -24,85 +25,105 @@ struct AdressView: View {
                 Text("Wunschadresse")
             }
         }
-        VStack(alignment: .leading) {
-            HStack {
-                Image(systemName: "largecircle.fill.circle")
-                    .padding(.trailing, 8)
-                VStack (alignment: .leading){
-                    HStack {
-                        Text(viewModel.user.firstName)
-                        Text(viewModel.user.secondName)
-                    }
-                    HStack {
-                        Text(viewModel.user.adress)
-                        Text(viewModel.user.houseNumber)
-                    }
-                    HStack {
-                        Text("\(viewModel.user.plz),")
-                        Text(viewModel.user.location)
+        ScrollView {
+            VStack/*(alignment: .leading)*/ {
+                HStack {
+                    Image(systemName: "largecircle.fill.circle")
+                        .padding(.trailing, 8)
+                    VStack (alignment: .leading){
+                        HStack {
+                            Text(viewModel.user.firstName)
+                            Text(viewModel.user.secondName)
+                        }
+                        HStack {
+                            Text(viewModel.user.adress)
+                            Text(viewModel.user.houseNumber)
+                        }
+                        HStack {
+                            Text("\(viewModel.user.plz),")
+                            Text(viewModel.user.location)
+                        }
                     }
                 }
-            }
-            HStack {
+                HStack {
+                    
                     Button {
-                        //Placeholder
+                        withAnimation(.easeInOut(duration: 0.25)) {
+                            showTextFields.toggle()
+                        }
                     } label: {
-                        Image(systemName: "plus")
-                            .padding(.trailing, 8)
-                        Text("Neue Adresse hinzufügen")
+                        if !showTextFields{
+                            Image(systemName: "plus")
+                                .padding(.trailing, 8)
+                            Text("Neue Adresse hinzufügen")
+                        }
                         
                     }
                     .tint(.primary)
+                    
+                }
+                .padding()
+                
+                if showTextFields {
+                    VStack {
+                        TextField("Vorname", text: .constant(""))
+                            .padding()
+                            .textFieldStyle(.plain)
+                            .border(.primary.opacity(0.4))
+                        
+                        TextField("Nachname", text: .constant(""))
+                            .padding()
+                            .textFieldStyle(.plain)
+                            .border(.primary.opacity(0.4))
+                        
+                        TextField("Adresse", text: .constant(""))
+                            .padding()
+                            .textFieldStyle(.plain)
+                            .border(.primary.opacity(0.4))
+                        
+                        TextField("Hausnummer", text: .constant(""))
+                            .padding()
+                            .textFieldStyle(.plain)
+                            .border(.primary.opacity(0.4))
+                        
+                        TextField("PLZ", text: .constant(""))
+                            .padding()
+                            .textFieldStyle(.plain)
+                            .border(.primary.opacity(0.4))
+                        
+                        TextField("Ort", text: .constant(""))
+                            .padding()
+                            .textFieldStyle(.plain)
+                            .border(.primary.opacity(0.4))
+                        
+                        VStack {
+                            Button("Stornieren") {
+                                withAnimation(.easeInOut(duration: 0.25)) {
+                                    showTextFields.toggle()
+                                }
+                            }
+                            .frame(maxWidth: 400, minHeight: 50)
+                            .border(.primary)
+                            .foregroundStyle(.primary)
+                            
+                            Button("Speichern") {
+                                withAnimation(.easeInOut(duration: 0.25)) {
+                                    showTextFields.toggle()
+                                }
+                            }
+                            .frame(maxWidth: 400, minHeight: 50)
+                            .foregroundStyle(.white)
+                            .background(Color.primary)
+                        }
+                        .padding(.top, 30)
+                    }
+                    .padding()
+                }
                 
             }
             .padding()
-            
-            VStack {
-                TextField("Vorname", text: .constant(""))
-                    .padding()
-                    .textFieldStyle(.plain)
-                    .border(.primary.opacity(0.4))
-                
-                TextField("Nachname", text: .constant(""))
-                    .padding()
-                    .textFieldStyle(.plain)
-                    .border(.primary.opacity(0.4))
-
-                TextField("Adresse", text: .constant(""))
-                    .padding()
-                    .textFieldStyle(.plain)
-                    .border(.primary.opacity(0.4))
-
-                TextField("Hausnummer", text: .constant(""))
-                    .padding()
-                    .textFieldStyle(.plain)
-                    .border(.primary.opacity(0.4))
-
-                TextField("PLZ", text: .constant(""))
-                    .padding()
-                    .textFieldStyle(.plain)
-                    .border(.primary.opacity(0.4))
-
-                TextField("Ort", text: .constant(""))
-                    .padding()
-                    .textFieldStyle(.plain)
-                    .border(.primary.opacity(0.4))
-                
-                Button("Stornieren") {
-                    
-                }
-                Button("Speichern") {
-                    
-                }
-                .frame(maxWidth: 400, maxHeight: 60)
-                .border(.primary)
-                .foregroundStyle(.primary)
-            }
-            .padding()
-
+            .frame(maxWidth: .infinity)
         }
-        .padding()
-        .frame(maxWidth: .infinity)
     }
 }
 
