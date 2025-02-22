633395fa44a49bc7097a0191c33ab96b422a67d9
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-02-22 20:13:08 +0100

    onSubmit fixxed in registerView

--- Fast-Shop/Fast-Shop/Views/Auth/RegisterViewSheet.swift
+++ Fast-Shop/Fast-Shop/Views/Auth/RegisterViewSheet.swift
@@ -50,7 +50,12 @@ struct RegisterViewSheet: View {
                     .padding()
                     .border(.primary)
                     .onSubmit {
-                        authViewModel.register()
+                        if authViewModel.acceptTerms {
+                            authViewModel.register()
+                        } else {
+                            errorHandler.errorMessage = "Bitte akzeptieren Sie die AGB"
+                            errorHandler.showError = true
+                        }
                     }
             }
                   
